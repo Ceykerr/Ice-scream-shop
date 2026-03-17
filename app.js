4477f9eaef7a0b8eb7081488c58e2b0c7d9c4ac5
Natasha Rosa Vargas <natasha@Natashas-MacBook-Pro.local>
2026-03-16 20:38:32 -0400

    Templates and cards

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname + "/public")));
-app.set("views", path.join(__dirname, "src/views"));
+// Motor de vistas
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
 
+// Archivos estáticos (CSS, JS, IMG)
+app.use(express.static(path.join(__dirname, "public")));
 
+// Rutas
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { titulo: "mi titulo dinámico" });
 });
+
 app.get("/:page", (req, res) => {
     const page = req.params.page;
     res.render(page);
 });
 
-app.listen(port, () => {console.log(`Server is running on http://localhost:${port}`)});
+// Servidor
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
